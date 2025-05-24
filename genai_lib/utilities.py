0ebde1116d5e5b0f78930e70b47caeac284ec399
Ralf <91786968+ralf-42@users.noreply.github.com>
2025-05-24 18:59:31 +0200

    Update 05.2025

--- genai_lib/utilities.py
+++ genai_lib/utilities.py
@@ -84,26 +84,6 @@ def get_ipinfo():
         print("Fehler beim Abrufen der IP-Informationen:", e)
 
 
-    
-def setup_api_keys_alt(key_names):
-    """
-    Setzt angegebene API-Keys aus Google Colab userdata als Umgebungsvariablen.
-
-    Args:
-        key_names (list[str]): Liste der Namen der API-Keys (z. B. ["OPENAI_API_KEY", "HF_TOKEN"]).
-
-    Hinweis:
-        Die API-Keys werden direkt in die Umgebungsvariablen geschrieben,
-        aber NICHT zurückgegeben, um unbeabsichtigte Sichtbarkeit zu vermeiden.
-    """
-    from google.colab import userdata
-    from os import environ
-
-    for key in key_names:
-        value = userdata.get(key)
-        if value:
-            environ[key] = value
-
 def setup_api_keys(key_names, create_globals=True):
     """
     Setzt angegebene API-Keys aus Google Colab userdata als Umgebungsvariablen
